ecac5656458644d6a5b7bbb813383a62f73820ee
shantha7kumar <Shantha7thiru@gmail.com>
2023-06-21 14:10:50 +0000

    commited

--- springapp/src/main/java/com/example/springapp/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springapp.repository;
-
-import com.example.springapp.model.PaymentData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<PaymentData, Long> {
-}
