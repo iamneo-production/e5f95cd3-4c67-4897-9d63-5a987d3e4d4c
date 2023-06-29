56e5963ad15b693a8d2a6c941a586d786f304709
shantha7kumar <Shantha7thiru@gmail.com>
2023-06-29 09:55:22 +0000

    committed

---
+++ springapp/src/main/java/com/example/springapp/service/PaymentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.springapp.service;
+
+import com.example.springapp.model.Payment;
+import com.example.springapp.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Override
+    public Payment savePayment(Payment payments) {
+        return paymentRepository.save(payments);
+    }
+
+    @Override
+    public List<Payment> getAllPayments() {
+        return paymentRepository.findAll();
+    }
+}
