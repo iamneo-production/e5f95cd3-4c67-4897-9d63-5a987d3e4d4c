cd7b11a475e1733c58ebe146237222b4be3041e6
udaychand348 <udaychandchoulla@gmail.com>
2023-07-12 03:38:40 +0000

    springapp testcase

---
+++ springapp/src/main/java/com/example/springapp/repository/TicketsRepository.java
@@ -0,0 +1,13 @@
+package com.example.springapp.repository;
+
+import com.example.springapp.model.Ticket;
+
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TicketsRepository extends JpaRepository<Ticket,Long>
+{
+  List<Ticket>  findTicketByEventId(Long id);
+  List<Ticket>  findTicketByAttendeeId(Long id);
+}
