42b8ed27a03e2c0249a6fb23a6653a62b0caf615
Lokeswarreddy240302 <lokeswarreddy2403@gmail.com>
2023-07-28 08:23:00 +0000

    Test Cases

---
+++ springapp/src/main/java/com/example/springapp/repository/TicketSalesRepository.java
@@ -0,0 +1,11 @@
+package com.example.springapp.repository;
+
+import com.example.springapp.model.TicketSales;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TicketSalesRepository extends JpaRepository<TicketSales,Long>
+{
+
+}
